1cb72fb6ce35b17cf4250b4d44c28ed838ca81f4
Hendy Gomodo <hendy.lazuardi@gomodo.tech>
2019-11-04 10:57:29 +0700

    update data

--- src/app/data.ts
+++ src/app/data.ts
@@ -15,7 +15,7 @@ export const data = [
   },
   {
     id : 2,
-    img : 'https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    img : 'http://onlinetao.ru/blog/wp-content/uploads/2015/07/hq-wallpapers_ru_nature_35955_1920x1200.jpg',
     name : 'Agung',
     last_name : 'Lazuardi',
     dummy: 'How to make mac',
@@ -30,7 +30,7 @@ export const data = [
   },
   {
     id : 3,
-    img : 'https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    img : 'https://thumbs.dreamstime.com/z/%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%B2-%D0%BC%D0%BE%D1%81%D1%82%D0%B0-%D0%BA-%D1%82%D1%80%D0%BE%D0%BF%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%BC%D1%83-21832640.jpg',
     name : 'Hapsah',
     last_name : 'Lazuardi',
     dummy: 'How to make ATM',
@@ -47,7 +47,7 @@ export const data = [
   },
   {
     id : 4,
-    img : 'https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    img : 'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     name : 'Qory',
     dummy: 'How to make money',
     activities: 'commented',
@@ -63,7 +63,7 @@ export const data = [
   },
   {
     id : 5,
-    img : 'https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    img : 'https://www.belightsoft.com/products/imagetricks/img/intro-video-poster@2x.jpg',
     name : 'Gore',
     dummy: 'How to make fired rice',
     activities: 'commented',
@@ -79,7 +79,7 @@ export const data = [
   },
   {
     id : 6,
-    img : 'https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    img : 'https://analyticsindiamag.com/wp-content/uploads/2018/12/image.jpg',
     name : 'Bryan',
     last_name : 'Lazuardi',
     dummy: 'How to make code',
